16997e3e2695e4067be869c31ac62d1b256f790c
sosnus <sosnus15@gmail.com>
2017-10-20 13:30:15 +0200

    Podstawowy kod

---
+++ Linefollower/Linefollower.ino
@@ -0,0 +1,65 @@
+#define L_PWM 5
+#define L_DIR 4
+#define R_PWM 6
+#define R_DIR 9
+#define PWM_MAX 165
+ 
+#define R_LINE_SENSOR A0
+#define L_LINE_SENSOR A1
+#define BUZZER 10
+#define LED 13
+ 
+void setup() {
+  //Konfiguracja pinow od mostka H
+  pinMode(L_DIR, OUTPUT);
+  pinMode(R_DIR, OUTPUT);
+  pinMode(L_PWM, OUTPUT);
+  pinMode(R_PWM, OUTPUT);
+ 
+  //Konfiguracja pozostalych elementow
+  pinMode(BUZZER, OUTPUT);
+  digitalWrite(BUZZER, 0); //Wylaczenie buzzera  
+  pinMode(LED, OUTPUT); 
+  digitalWrite(LED, 0); //Wylaczenie diody
+ 
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  int odczytLewy = analogRead(L_LINE_SENSOR);
+ 
+  Serial.print("Lewy czujnik: ");
+  Serial.println(odczytLewy);
+  delay(250);
+}
+ 
+void leftMotor(int V) {
+  if (V > 0) { //Jesli predkosc jest wieksza od 0 (dodatnia)
+    V = map(V, 0, 100, 0, PWM_MAX);
+    digitalWrite(L_DIR, 0); //Kierunek: do przodu
+    analogWrite(L_PWM, V); //Ustawienie predkosci 
+  } else {
+    V = abs(V); //Funkcja abs() zwroci wartosc V  bez znaku
+    V = map(V, 0, 100, 0, PWM_MAX);
+    digitalWrite(L_DIR, 1); //Kierunek: do tyłu
+    analogWrite(L_PWM, V); //Ustawienie predkosci    
+  }
+}
+ 
+void rightMotor(int V) {
+  if (V > 0) { //Jesli predkosc jest wieksza od 0 (dodatnia)
+    V = map(V, 0, 100, 0, PWM_MAX);
+    digitalWrite(R_DIR, 0); //Kierunek: do przodu
+    analogWrite(R_PWM, V); //Ustawienie predkosci 
+  } else {
+    V = abs(V); //Funkcja abs() zwroci wartosc V  bez znaku
+    V = map(V, 0, 100, 0, PWM_MAX);
+    digitalWrite(R_DIR, 1); //Kierunek: do tyłu
+    analogWrite(R_PWM, V); //Ustawienie predkosci    
+  }
+}
+ 
+void stopMotors() {
+  analogWrite(L_PWM, 0); //Wylaczenie silnika lewego
+  analogWrite(R_PWM, 0); //Wylaczenie silnika prawego
+}
